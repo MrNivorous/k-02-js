a40a6895e3893aba9b9d87b520994be90768ad5e
MrNivorous <karn.sood@gmail.com>
2019-08-15 12:14:20 +0700

    map1

--- 01-loops.js
+++ 01-loops.js
@@ -5,13 +5,24 @@ let mix = [5, 'David', {name:'Ella'}, 23, 'Frank']
 const logNumbers = (n) => {
 	n.forEach(x => {
 	console.log(x)
-})}
+})
+}
 
 logNumbers(numbers)
 
 const logNames = (names) => {
 	names.forEach(n => {
 	console.log(n.name)
-})}
+})
+}
 
 logNames(students)
+
+const add1 = (n) => {
+	console.log(n.map(x => {
+	return x + 1
+		})
+	)
+}
+
+add1(numbers)
