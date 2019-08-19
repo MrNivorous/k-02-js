3e417b8ea550efb3d2d88dd2e77f82fada2ebd80
MrNivorous <karn.sood@gmail.com>
2019-08-19 14:26:02 +0700

    020502

--- 05-classes.js
+++ 05-classes.js
@@ -1,6 +1,6 @@
 // 020501
 
-let buildCar = class Car {
+class Car {
 	constructor(make, model, color) {
 		this.make = make
 		this.model = model
@@ -8,5 +8,31 @@ let buildCar = class Car {
 	}
 }
 
-let newCar = new buildCar('Ferrari', 'Testarossa', 'blue')
-console.log(newCar)
+let buildCar = (make, model, color) => {
+	let newCar = new Car(make, model, color)
+	return newCar
+}
+
+
+console.log(buildCar('Ferrari','Testarossa','blue'))
+
+// 020502
+let names = ['Alice', 'Bernard', 'Catherine', 'David']
+let jobs = ['Accountant', 'Biz Manager', 'Cleaner', 'Director']
+
+class Employee {
+	constructor(name, job) {
+		this.name = name
+		this.job = job
+		}
+	}
+
+
+
+const makeEmployees = (nameArray, jobArray) => {
+	return nameArray.map((name, i) => {
+		return new Employee(name,jobArray[i])
+})
+}
+
+console.log(makeEmployees(names,jobs))
