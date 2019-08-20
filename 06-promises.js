27b854c249893438a7dd5bdd8b49edcd99b82a99
MrNivorous <karn.sood@gmail.com>
2019-08-20 09:02:59 +0700

    promises done

--- 06-promises.js
+++ 06-promises.js
@@ -42,3 +42,55 @@ getUp().then(res => {
 		})
 	})
 })
+
+let makeCoffee = () => {
+	return new Promise((resolve, reject) => {
+		return setTimeout(() => {resolve('coffee')}, 10000)
+	})
+}
+
+let makeEggs = () => {
+	return new Promise((resolve, reject) => {
+		 return setTimeout(() => {resolve('eggs')}, 5000)
+	})
+}
+
+let makeToast = () => {
+	return new Promise((resolve, reject) => {
+		return setTimeout(() => {resolve('toast')}, 6000)
+	})
+}
+
+let makeBacon = () => {
+	return new Promise((resolve, reject) => {
+		return	setTimeout(() => {resolve('bacon')}, 8000)
+	})
+}
+
+Promise.all([makeCoffee(), makeEggs(), makeToast(), makeBacon()]).then(res => console.log(res))
+
+// class Horse {
+// 	constructor(name) {
+// 		this.name = name
+// 	}
+// }
+
+let Speedy = () => {
+	return new Promise((resolve, reject) => {
+		return setTimeout(() => {resolve('Speedy')}, Math.random() * 4000)
+	})
+}
+
+let Jackpot = () => {
+	return new Promise((resolve, reject) => {
+		return setTimeout(() => {resolve('Jackpot')}, Math.random() * 4000)
+	})
+}
+
+let Red = () => {
+	return new Promise((resolve, reject) => {
+		return setTimeout(() => {resolve('Red')}, Math.random() * 4000)
+	})
+}
+
+Promise.race([Speedy(), Jackpot(), Red()]).then(res => console.log(res))
